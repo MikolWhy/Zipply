007a765fb208243d4c9609357bc5644e9ee77c22
Michael Yik <Michael.yik7@gmail.com>
2025-12-22 19:05:53 -0500

    Install dependencies and restructure to src/ directory. Updated to next@16.1.1 to fix vulnerability

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //double renders in dev and warns about deprecated APIs
+  reactStrictMode: true,
 };
 
 export default nextConfig;
